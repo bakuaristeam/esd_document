06144b6d2b89437a32b444ebede0e1001f7b0e7c
Seymur <seymurhuseynov@gmail.com>
2019-08-29 16:52:12 +0400

    en son versiya idOrg org la evez olunmasi,applicant yeni fildlerin elavesi 29.08.2019

---
+++ src/main/java/com/aris/ESD_Document/Service/internal/DocumentResponse.java
@@ -0,0 +1,64 @@
+package com.aris.ESD_Document.Service.internal;
+
+import com.aris.ESD_Document.db.entities.Document;
+
+public class DocumentResponse {
+
+    private int serverCode;
+    private String serverMessage;
+    private String statusMessage;
+    private Document document;
+
+
+    public DocumentResponse(int serverCode, String serverMessage, String statusMessage, Document document) {
+        this.serverCode = serverCode;
+        this.serverMessage = serverMessage;
+        this.statusMessage = statusMessage;
+        this.document = document;
+    }
+
+    public DocumentResponse() {
+    }
+
+    @Override
+    public String toString() {
+        return "DocumentResponse{" +
+                "serverCode=" + serverCode +
+                ", serverMessage='" + serverMessage + '\'' +
+                ", statusMessage='" + statusMessage + '\'' +
+                ", document=" + document +
+                '}';
+    }
+
+    public int getServerCode() {
+        return serverCode;
+    }
+
+    public void setServerCode(int serverCode) {
+        this.serverCode = serverCode;
+    }
+
+    public String getServerMessage() {
+        return serverMessage;
+    }
+
+    public void setServerMessage(String serverMessage) {
+        this.serverMessage = serverMessage;
+    }
+
+    public String getStatusMessage() {
+        return statusMessage;
+    }
+
+    public void setStatusMessage(String statusMessage) {
+        this.statusMessage = statusMessage;
+    }
+
+    public Document getDocument() {
+        return document;
+    }
+
+    public void setDocument(Document document) {
+        this.document = document;
+    }
+}
---
+++ src/main/java/com/aris/ESD_Document/Service/internal/crud/RequestUpdateCombineDoc.java
@@ -0,0 +1,60 @@
+package com.aris.ESD_Document.Service.internal.crud;
+
+public class RequestUpdateCombineDoc {
+
+    private long idCombineDocument;
+    private long idDocMain;
+    private long idDocCombined;
+    private  int isActive;
+    public RequestUpdateCombineDoc() {
+    }
+
+    public RequestUpdateCombineDoc(long idCombineDocument, long idDocMain, long idDocCombined, int isActive) {
+        this.idCombineDocument = idCombineDocument;
+        this.idDocMain = idDocMain;
+        this.idDocCombined = idDocCombined;
+        this.isActive = isActive;
+    }
+
+    @Override
+    public String toString() {
+        return "RequestUpdateCombineDoc{" +
+                "idCombineDocument=" + idCombineDocument +
+                ", idDocMain=" + idDocMain +
+                ", idDocCombined=" + idDocCombined +
+                ", isActive=" + isActive +
+                '}';
+    }
+
+    public long getIdCombineDocument() {
+        return idCombineDocument;
+    }
+
+    public void setIdCombineDocument(long idCombineDocument) {
+        this.idCombineDocument = idCombineDocument;
+    }
+
+    public long getIdDocMain() {
+        return idDocMain;
+    }
+
+    public void setIdDocMain(long idDocMain) {
+        this.idDocMain = idDocMain;
+    }
+
+    public long getIdDocCombined() {
+        return idDocCombined;
+    }
+
+    public void setIdDocCombined(long idDocCombined) {
+        this.idDocCombined = idDocCombined;
+    }
+
+    public int getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(int isActive) {
+        this.isActive = isActive;
+    }
+}
